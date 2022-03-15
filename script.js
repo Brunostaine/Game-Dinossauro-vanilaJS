9d064d55a35274af7692b864eb358a82ccbb7f3a
Bruno Staine <brunostaine@hotmail.com>
2022-03-15 20:29:38 -0300

    :bug: Bug de pular varias vezes arrumado

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const dino = document.querySelector('.dino')
 
+let sePulando = false
+
 function dandleKeyUp (event) {
     if(event.keyCode === 32){
-        pular()
+        if(!sePulando){
+            pular()
+        }
     }
 }
 
 function pular () {
     let position = 0
+
+    sePulando = true
 
     let upInterval = setInterval (() => {
         if (position >= 150) {
@@ -16,6 +22,7 @@ function pular () {
     let downInterval = setInterval(() => {
         if (position <= 0) {
             clearInterval(downInterval)
+            sePulando = false
         } else {
             position -= 20
             dino.style.bottom = position + 'px'
