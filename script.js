ec908b16b1c8c42ba86c1af3d9371c55c0b6f06f
Bruno Staine <brunostaine@hotmail.com>
2022-03-15 20:24:32 -0300

    :sparkles: Função de pular

---
+++ script.js
@@ -0,0 +1,33 @@
+const dino = document.querySelector('.dino')
+
+function dandleKeyUp (event) {
+    if(event.keyCode === 32){
+        pular()
+    }
+}
+
+function pular () {
+    let position = 0
+
+    let upInterval = setInterval (() => {
+        if (position >= 150) {
+            clearInterval(upInterval)
+
+    let downInterval = setInterval(() => {
+        if (position <= 0) {
+            clearInterval(downInterval)
+        } else {
+            position -= 20
+            dino.style.bottom = position + 'px'
+        }
+    }, 20 )        
+        }else {
+            position += 20
+            dino.style.bottom = position + 'px'
+        }
+    }, 20 )
+
+
+}
+
+document.addEventListener('keyup', dandleKeyUp)
